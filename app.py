8350d68eb5fc84f7336d22cd2d75a23bd60448e9
Ubuntu <ubuntu@ip-172-31-28-15.us-east-2.compute.internal>
2020-02-10 02:38:08 +0000

    query of title, url, and instructions

--- app.py
+++ app.py
@@ -5,11 +5,19 @@ url = "https://api.spoonacular.com/recipes/random?apiKey=1b2c6332c94c4f27ae541fb
 response = requests.get(url)
 json = response.json()
 
+title = str(json['recipes'][0]['title'])
+url = str(json['recipes'][0]['sourceUrl'])
+instructions = json['recipes'][0]['analyzedInstructions'][0]['steps']
+steps = []
+
+for i in range(len(instructions)):
+    steps.append(instructions[i]['step'])
+
 app = flask.Flask(__name__)
 @app.route('/') 
 
 def index(): 
-    return str(json)
+    return title
 
 app.run(
     port=int(os.getenv('PORT', 8080)),
