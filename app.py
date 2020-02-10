0ea1d26f9dcb3d785bbad5168630aebcb34d2a2d
Ubuntu <ubuntu@ip-172-31-28-15.us-east-2.compute.internal>
2020-02-10 14:21:04 +0000

    edited html and app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import json, flask, os
 import requests_oauthlib, requests
 
-url = "https://api.spoonacular.com/recipes/random?apiKey=1b2c6332c94c4f27ae541fbef839174f"
+url = "https://api.spoonacular.com/recipes/random?number=20&apiKey=1b2c6332c94c4f27ae541fbef839174f"
 response = requests.get(url)
 json = response.json()
 
 title = str(json['recipes'][0]['title'])
 url = str(json['recipes'][0]['sourceUrl'])
-instructions = json['recipes'][0]['instructions']
+instructions = json['recipes'][0]['instructions'][0]['steps']
+steps = []
+
+for i in range(len(instructions)):
+    steps.append(instructions[i]['step'])
 
 app = flask.Flask(__name__)
 @app.route('/') 
@@ -17,7 +21,7 @@ def index():
         "index.html", 
         title = title, 
         url = url,
-        steps = instructions)
+        steps = steps)
 
 app.run(
     port=int(os.getenv('PORT', 8080)),
