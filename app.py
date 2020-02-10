f5664e057728c527440f1d2ef44c8fce6b082c29
Ubuntu <ubuntu@ip-172-31-28-15.us-east-2.compute.internal>
2020-02-10 02:06:11 +0000

    api request with random endpoint

---
+++ app.py
@@ -0,0 +1,18 @@
+import random, json, flask, os
+import requests_oauthlib, requests
+
+url = "https://api.spoonacular.com/recipes/random?apiKey=1b2c6332c94c4f27ae541fbef839174f"
+response = requests.get(url)
+json = response.json()
+
+app = flask.Flask(__name__)
+@app.route('/') 
+
+def index(): 
+    return str(json)
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug = True
+    )
