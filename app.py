1559aae89d05070370e293ffe5259f6117b80234
Ubuntu <ubuntu@ip-172-31-28-15.us-east-2.compute.internal>
2020-02-11 22:09:40 +0000

    change to html, api keys

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ import json, flask, os
 import requests_oauthlib, requests
 import random
 
+#API KEYS
+spoon_key = str(os.getenv('spoon_api_key'))
+twit_key = os.getenv('twit_api_key')
+twit_secret_key = str(os.getenv('twit_api_secret_key'))
+twit_token = str(os.getenv('twit_api_token'))
+twit_secret_token = str(os.getenv('twit_api_secret_token'))
+
 foodurl = "https://api.spoonacular.com/recipes/random?cuisine=african?number=15&apiKey=b37e2c0b29534d18913727984e0481c9"
 response = requests.get(foodurl)
-json = response.json()
+json_food = response.json()
 
-title = str(json['recipes'][0]['title'])
-cooktime = str(json['recipes'][0]['readyInMinutes'])
-url = str(json['recipes'][0]['sourceUrl'])
-instructions = json['recipes'][0]['instructions']
-ingredients_arrary = json['recipes'][0]["extendedIngredients"]
+title = str(json_food['recipes'][0]['title'])
+cooktime = str(json_food['recipes'][0]['readyInMinutes'])
+url = str(json_food['recipes'][0]['sourceUrl'])
+instructions = json_food['recipes'][0]['instructions']
+ingredients_arrary = json_food['recipes'][0]["extendedIngredients"]
 ingredients = []
+
 x = random.randint(0,4)
 y = random.randint(4,9)
 z = random.randint(9,14)
@@ -20,19 +28,18 @@ for i in range(len(ingredients_arrary)):
     ingredients.append(ingredients_arrary[i]["original"])
 
 twitterurl = "https://api.twitter.com/1.1/search/tweets.json?q=african%20africanfood&lang=en&result_type=mixed"
-
 oauth = requests_oauthlib.OAuth1(
-    "8RcjcUSIFpAZUPOs12MChqe3Y", 
-    "WpPz4e8iTYEwY1aHvwcZCb3v2QdIHjfLvcwwxpRqCPHQMmv6ta",
-    "1223247877645963265-Tcl68pUZcpUa6M43RMB4RuS8URIlM8",
-    "G4AgehG0QbHwrNhUBuRbM7FJylLERBqrkozbVOUuRD7uK"
-)
-
+    "3KnuswndfL6QTaumOdSIinn28", 
+    "zECfYUanM0TqociMqFojTYD0hoAQGU4wywERUU44Ux8CARKQZM",
+    "1223247877645963265-6JO3MkFHj6sLTSpy1Htg5jcmH4FpCm",
+    "grVs0ZjlduVocPsKS1FpHeUXnddD45TSc32y0RwHXdr7D"
+    )
 response = requests.get(twitterurl, auth=oauth)
-jsonbody = response.json()
-tweet1 = str(jsonbody["statuses"][x]["text"])
-tweet2 = str(jsonbody["statuses"][y]["text"])
-tweet3 = str(jsonbody["statuses"][z]["text"])
+tweets_json = response.json()
+
+tweet1 = str(tweets_json["statuses"][x]["text"])
+tweet2 = str(tweets_json["statuses"][y]["text"])
+tweet3 = str(tweets_json["statuses"][z]["text"])
 
 app = flask.Flask(__name__)
 @app.route('/') 
@@ -42,11 +49,11 @@ def index():
         "index.html", 
         title = title, 
         url = url,
-        instructions = instructions,
-        ingredients = ingredients,
         tweet1 = tweet1,
         tweet2 = tweet2,
         tweet3 = tweet3,
+        instructions = instructions,
+        ingredients = ingredients,
         cooktime = cooktime
         )
 
