614c27c379a7459d21ab45bcc27af4f6a31f75e1
Ubuntu <ubuntu@ip-172-31-28-15.us-east-2.compute.internal>
2020-02-11 04:34:49 +0000

    twitter api works, html displays all necessary info

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import json, flask, os
 import requests_oauthlib, requests
 
-url = "https://api.spoonacular.com/recipes/random?number=20&apiKey=1b2c6332c94c4f27ae541fbef839174f"
-response = requests.get(url)
+foodurl = "https://api.spoonacular.com/recipes/random?cuisine=african?number=15&apiKey=b37e2c0b29534d18913727984e0481c9"
+response = requests.get(foodurl)
 json = response.json()
 
 title = str(json['recipes'][0]['title'])
+cooktime = str(json['recipes'][0]['readyInMinutes'])
 url = str(json['recipes'][0]['sourceUrl'])
-instructions = json['recipes'][0]['instructions'][0]['steps']
-steps = []
+instructions = json['recipes'][0]['instructions']
+ingredients = json['recipes'][0]["extendedIngredients"]
 
-for i in range(len(instructions)):
-    steps.append(instructions[i]['step'])
+
+twitterurl = "https://api.twitter.com/1.1/search/tweets.json?q=african%20africanfood&lang=en&result_type=mixed"
+
+oauth = requests_oauthlib.OAuth1(
+    "8RcjcUSIFpAZUPOs12MChqe3Y", 
+    "WpPz4e8iTYEwY1aHvwcZCb3v2QdIHjfLvcwwxpRqCPHQMmv6ta",
+    "1223247877645963265-Tcl68pUZcpUa6M43RMB4RuS8URIlM8",
+    "G4AgehG0QbHwrNhUBuRbM7FJylLERBqrkozbVOUuRD7uK"
+)
+
+response = requests.get(twitterurl, auth=oauth)
+jsonbody = response.json()
+tweets = str(jsonbody["statuses"][0]["text"])
 
 app = flask.Flask(__name__)
 @app.route('/') 
@@ -21,7 +33,11 @@ def index():
         "index.html", 
         title = title, 
         url = url,
-        steps = steps)
+        instructions = instructions,
+        ingredients = ingredients,
+        tweets = tweets,
+        cooktime = cooktime
+        )
 
 app.run(
     port=int(os.getenv('PORT', 8080)),
